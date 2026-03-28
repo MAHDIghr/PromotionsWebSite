e8c1cb7b063d5137d282c5763df8e423b814efc3
Mahdi gherbi <145656572+MAHDIghr@users.noreply.github.com>
2026-03-28 23:22:10 +0100

    Delete staticfiles directory

--- staticfiles/js/faq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(item => {
-    item.addEventListener('click', () => {
-        const parent = item.parentElement;
-        parent.classList.toggle('active');
-    });
-});
